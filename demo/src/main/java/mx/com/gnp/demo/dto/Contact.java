208848de167065ed2a6999d08dca95bd536e433a
ibravo <ibravo@L1048727.GNP.MX>
2019-07-03 10:40:38 -0500

    es una tercera prueba

--- demo/src/main/java/mx/com/gnp/demo/dto/Contact.java
+++ demo/src/main/java/mx/com/gnp/demo/dto/Contact.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 @Entity
 public class Contact implements Serializable {
 	
-/**Este es un comentario **/	
+/** Es la segunda prueba de sincronizacion **/
 	private static final long serialVersionUID = 4894729030347835498L;
 	 @Id
 	 @GeneratedValue
