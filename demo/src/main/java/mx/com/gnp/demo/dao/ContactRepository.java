7f3ea11d9ea22c77739d88ac615a82b4e7354100
Israel <Israel@Israel-mini>
2020-01-25 13:20:40 -0600

    es lo del repositorio

--- demo/src/main/java/mx/com/gnp/demo/dao/ContactRepository.java
+++
@@ -1,8 +0,0 @@
-package mx.com.gnp.demo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import mx.com.gnp.demo.dto.Contact;
-
-public interface ContactRepository extends JpaRepository<Contact, Long> {
-
-}
--- demo/src/main/java/mx/com/gnp/demo/dao/ContactoRepositorio.java
+++
@@ -1,6 +0,0 @@
-package mx.com.gnp.demo.dao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import mx.com.gnp.demo.dto.*;
-public interface ContactoRepositorio extends JpaRepository<Contacto, Long>{
-
-}
